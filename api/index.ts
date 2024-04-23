a5b909e7c9c22b0b049164edae64f69156fd5758
Giulio Zanchetta <zank@outlook.it>
2024-04-23 18:41:01 +0200

    Refactor API server for serverless deployment
    Significant changes include:
    - Removed dotenv and body-parser dependencies
    - Added serverless-http dependency to support AWS Lambda functions
    - Simplified the file reading process in '/test/keen-slider' endpoint by using fs.readFile instead of creating a read stream
    - Replaced app.listen with module.exports.handler for serverless deployment

--- api/index.ts
+++ api/index.ts
@@ -1,39 +1,33 @@
-require('dotenv').config();
-
 const express = require('express');
 const app = express();
+const serverless = require('serverless-http');
 
-const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
 const { PassThrough } = require('stream');
 
-// Create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
 app.use(express.static('public'));
 
 app.get('/test/keen-slider', (req, res) => {
-	// Create a pass-through stream to combine file and extra data
-    const pass = new PassThrough();
-
-    // Send the combined stream as response
-    pass.pipe(res);
-
-    // Function to send file with additional data
     setTimeout(() => {
-        // Read the actual file
-		res.setHeader('Content-Type', 'text/javascript');
-        const fileStream = fs.createReadStream(path.join(__dirname, '..', 'components', 'keen-slider@6.8.6.min.js'));
-        fileStream.pipe(res, { end: false });
-        fileStream.on('end', () => {
-            res.write("\n/*" + "0".repeat(20 * 1024 * 1024) + "*/\n");  // 20MB of '0' characters as a JavaScript comment
-            res.end();
+        const filePath = path.join(__dirname, '..', 'components', 'keen-slider@6.8.6.min.js');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.status(500).send("Error reading file.");
+                return;
+            }
+
+            // Setting Content-Type
+            res.setHeader('Content-Type', 'text/javascript');
+
+            // Create a pass-through stream
+            const pass = new PassThrough();
+            pass.end(data + "\n/*" + "0".repeat(20 * 1024 * 1024) + "*/\n");
+
+            // Send the combined stream as response
+            pass.pipe(res);
         });
     }, 5000);
 });
 
-
-app.listen(3000, () => console.log('Server ready on port 3000.'));
-
-module.exports = app;
+module.exports.handler = serverless(app);
