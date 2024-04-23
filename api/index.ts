679d0f16f76e202a54e43d3b459428d819b0fa77
Giulio Zanchetta <zank@outlook.it>
2024-04-23 18:44:53 +0200

    Updated server configuration
    Removed the dependency on 'serverless-http' and updated the application to listen directly on port 3000. Also, modified the module export to return the app instance instead of a serverless handler.

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const serverless = require('serverless-http');
 
 const path = require('path');
 const fs = require('fs');
@@ -30,4 +29,6 @@ app.get('/test/keen-slider', (req, res) => {
     }, 5000);
 });
 
-module.exports.handler = serverless(app);
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
